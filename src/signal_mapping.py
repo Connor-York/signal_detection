d4729144d2258409a65416cd9f1fd601ee5a84b4
Connor <connor.york000@gmail.com>
2025-10-28 17:09:59 +0000

    Updated logging

--- src/signal_mapping.py
+++ src/signal_mapping.py
@@ -2,12 +2,13 @@
 
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped, Twist
-from std_msgs.msg import Float32
+from std_msgs.msg import String
 import csv
 import time 
 import datetime
 import serial
 import rospkg
+import json
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
@@ -24,6 +25,7 @@ initial_x = rospy.get_param("/amcl/initial_pose_x")
 initial_y = rospy.get_param("/amcl/initial_pose_y")
 
 current_pose = [initial_x, initial_y]
+patrol_status = [0,0] # start goal (0 assumed), patrol count (0 known)
 
 def pose_callback(msg):
     x = msg.pose.pose.position.x
@@ -31,16 +33,21 @@ def pose_callback(msg):
     global current_pose 
     current_pose = [x,y]
     
+def status_callback(msg):
+    global patrol_status
+    patrol_status = msg.data.strip().split(',')
+    
 
 
 if __name__ == '__main__':
     
     rospy.init_node("signal_mapping")
-    pose_subscriber = rospy.Subscriber("amcl_pose",PoseWithCovarianceStamped,pose_callback)
-    
+    pose_subscriber = rospy.Subscriber("/amcl_pose",PoseWithCovarianceStamped,pose_callback)
+    status_subscriber = rospy.Subscriber("/patrol_status",String,status_callback)
+
     with open(path, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['timestamp','pose_x','pose_y','xbee','lora'])
+        writer.writerow(['timestamp','curr_patrol','curr_goal','pose_x','pose_y','xbee','lora'])
         
         while not rospy.is_shutdown():
 
@@ -50,8 +57,12 @@ if __name__ == '__main__':
                 timestamp = round(time.time() - start_time,2)
                 
                 
-                datapack = [timestamp, current_pose[0], current_pose[1], int(arduino_output[0]), int(arduino_output[1])]
-                #rospy.loginfo(datapack)
+                datapack = [timestamp, patrol_status[1], patrol_status[0], current_pose[0], current_pose[1], int(arduino_output[0]), int(arduino_output[1])]
                 writer.writerow(datapack)
+                
+                current_time = time.time()
+                if current_time - prev_time >= 1:
+                    rospy.loginfo(datapack)
+                    prev_time = current_time
         
 
